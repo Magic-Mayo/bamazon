d9002b00dbb8d29930f2a61b0574803ea394bf5c
Magic-Mayo <mike.mayoii@gmail.com>
2019-08-07 10:22:14 -0700

    started function to start the order process

--- bamazon.js
+++ bamazon.js
@@ -1,3 +1,64 @@
 const mysql = require('mysql');
 const inq = require('inquirer');
+let orderTotal = [];
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "1fafp404",
+    database: "bamazon"
+  });
+
+connection.query('select * from products', (err,data)=>{
+    if(err) throw err;
+    // console.log(data);
+    userInput();
+})
+
+userInput = () => {
+    connection.query('select * from products', (err, data)=>{
+        if (err) throw err;
+        inq.prompt(
+            {
+                type: 'rawlist',
+                name: 'product',
+                choices: ()=>{
+                    const products = [];
+                    for (let i=0; i<data.length; i++){
+                        products.push(`${data[i].product_name}: $${data[i].price}`)
+                    } return products;
+                },
+                message: 'Which product would you like to purchase?',
+            },{
+                type: 'number',
+                message: 'Enter the quantity you wish to order: ',
+                name: 'quantity'
+            }).then(answer => {
+            orderTotal.push()
+            orderProduct(answer.product, answer.quantity);
+            inq.prompt(
+                {
+                    type: 'confirm',
+                    message: 'Would you like to order anything else?',
+                    name: 'ordermore',
+                    default: false
+                }).then(answer=>{
+                if (answer.ordermore){
+                    userInput()
+                } else {
+                    console.log(`Order Placed!  Your total is: $${total(orderTotal)}`);
+                    connection.end();
+                }
+            })
+        })
+    })
+}
+
+total = (input) => {
+    return input.reduce((a,b)=>a+b, 0)
+}
+
+orderProduct = (product, quantity) => {
+
+}

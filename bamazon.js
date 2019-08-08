ba49d37dca9a8d51f4e040fb44c7014dae455080
Magic-Mayo <mike.mayoii@gmail.com>
2019-08-07 21:54:04 -0700

    finished function for ordering. need to get function for backorder to work properly. having an issue with it not being able to get data inside the last promise

--- bamazon.js
+++ bamazon.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const inq = require('inquirer');
 let orderTotal = [];
 let products = [];
-let cart = [];
+let cartItems = [];
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -44,56 +44,55 @@ userInput = () => {
                 name: 'quantity'
             }
         ]).then(answer => {
-            // console.log(answer.product)
-            orderTotal.push((parseFloat(Math.round(data[parseInt(answer.product)-1].price*100)/100))*parseInt(answer.quantity));
-            cart.push(`${answer.quantity} ${data[parseInt(answer.product)-1].product_name}`)
-            // console.log(orderTotal)
-            // orderProduct(answer.product, answer.quantity);
-            inq.prompt([
-                {
-                    type: 'confirm',
-                    message: `You have ordered ${answer.quantity} ${data[parseInt(answer.product)-1].product_name}.  Is this correct?`,
-                    name: 'confirm',
-                    default: true
-                }
-            ]).then(answer=>{
-                if (answer.confirm){
-                    inq.prompt(
-                        {
-                            type: 'confirm',
-                            message: 'Would you like to order anything else?',
-                            name: 'ordermore',
-                            default: false
-                        }).then(answer=>{
-                        if (answer.ordermore){
-                            userInput();
-                        } else {
-                            console.log(`Order Placed!  Your total is: $${orderTotal.reduce(total)}`);
-                            connection.end();
-                        }
-                    })
-                } else {
-                    products.pop();
-                    cart.pop();
-                    orderTotal.pop();
+            // console.log(answer.quantity)
+            if (answer.quantity<=data[parseFloat(answer.product)].stock_quantity){
+                orderTotal.push((parseFloat(Math.round(data[parseInt(answer.product)-1].price*100)/100))*parseInt(answer.quantity));
+                cartItems.push(`${answer.quantity} ${data[parseInt(answer.product)-1].product_name}`);
+
+                connection.query('update products set ? where ?', 
+                [{stock_quantity: data[parseInt(answer.product)].stock_quantity-parseInt(answer.quantity)},
+                {product_name: data[parseInt(answer.product)].product_name}], (err)=>{
+                    
+                    if(err) throw err;
+                    // orderProduct(answer.product, answer.quantity);
                     inq.prompt([
                         {
-                            type: 'list',
-                            message: `You now have ${cart} in your cart. What you like to do?`,
-                            choices: ['Order more', 'Checkout'],
-                            name: 'cart'
+                            type: 'confirm',
+                            message: `You have ordered ${answer.quantity} ${data[parseInt(answer.product)-1].product_name}.  Is this correct?`,
+                            name: 'confirm',
+                            default: true
                         }
                     ]).then(answer=>{
-                        if (answer.cart === 'Order more'){
-                            userInput();
+                        if (answer.confirm){
+                            cart();
                         } else {
-                            console.log(`Order Placed!  Your total is: $${orderTotal.reduce(total)}`);
-                            connection.end();
+                            products.pop();
+                            cart.pop();
+                            orderTotal.pop();
+                            cart();
                         }
                     })
+                }); connection.end()
 
-                }
-            })
+            } else {
+                console.log(data[parseInt(answer.product-1)].stock_quantity)
+                console.log(parseInt(answer.quantity)-data[parseInt(answer.product-1)].stock_quantity)                    
+                inq.prompt([
+                    {
+                        type: 'confirm',
+                        message: `We're sorry but we currently only have ${data[parseInt(answer.product)-1].stock_quantity} ${data[parseInt(answer.product)-1].product_name} in stock.  Would you like to order ${parseFloat(data[parseInt(answer.product-1)].stock_quantity)} ${data[parseInt(answer.product)-1].product_name} now and place the other ${Math.abs(parseInt(data[parseInt(answer.product)-1].stock_quantity-parseInt(answer.quantity)))} on backorder?`,
+                        name: 'backorder',
+                        default: false
+                    }
+                ]).then(answer=>{
+                    if (answer.backorder){
+                        backOrder(parseInt(answer.quantity)-data[parseInt(answer.product-1)].stock_quantity, data[parseInt(answer.product-1)].product_name);
+                        cart();
+                    } else {
+                        cart();
+                    }
+                })
+            }
         })
     })
 }
@@ -103,3 +102,28 @@ total = (total, num) => {
 }
 
 // Have a 'backorder' function if customer wants to purchase more than what's in stock
+
+cart = () => {
+    inq.prompt([
+        {
+            type: 'list',
+            message: `You now have ${cartItems.join(', ')} in your cart for a total of ${orderTotal.reduce(total)}. What you like to do?`,
+            choices: ['Order more', 'Checkout'],
+            name: 'cart'
+        }
+    ]).then(answer=>{
+        if (answer.cart === 'Order more'){
+            userInput();
+        } else {
+            console.log(`Order Placed!  Your total is: $${orderTotal.reduce(total)}`);
+            connection.end();
+        }
+    })
+}
+
+backOrder = (orderQuan, product) => {
+    connection.query('update products set backorder=? stock_quantity=0 where product_name=?', [orderQuan, product], err =>{
+        if (err) throw err;
+        console.log(`You have placed ${orderQuan} ${product} on backorder.  We will inform you when they arrive!`)
+    })
+}

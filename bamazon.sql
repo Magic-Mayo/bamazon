e238d4e92b19478199a87d71a927ccf0353793d2
Magic-Mayo <mike.mayoii@gmail.com>
2019-08-07 21:55:26 -0700

    added a column for backorder quantity

--- bamazon.sql
+++ bamazon.sql
@@ -13,3 +13,6 @@ insert into products (product_name, department_name, price, stock_quantity)
 	values ('milk', 'dairy', 3.19, 100), ('toilet_paper', 'bathroom', 9.99, 50), ('lettuce', 'produce', .99, 30), ('pizza', 'frozen_food', 5.99, 49), 
     ('light_bulb', 'household', 1.93, 293),	('steak', 'meat', 15.42, 10), ('ketchup', 'condiments', 2.99, 64), ('bananas', 'produce', .49, 138), ('M&M', 'candy', 3.48, 58),
     ('AA_battery', 'household', 2.39, 39);
+
+alter table products add column backorder integer after stock_quantity;
+select * from products;
